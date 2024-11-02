f039b2c5fad33aaedccf0c2042ef5b8ea0852071
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-02 15:35:17 +0100

    add a test for the timetable get all route

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,22 @@
 import test from "node:test";
 import { createApp } from "./app";
-import  request from "supertest";
+import request from "supertest";
 import { deepEqual } from "node:assert";
 
 test("GET /", async () => {
- const app = createApp();
+  const app = createApp();
 
- const result = await request(app).get("/");
+  const result = await request(app).get("/");
 
- deepEqual(result.status, 200);
- deepEqual(result.body, {status: "ready"});
-})
+  deepEqual(result.status, 200);
+  deepEqual(result.body, { status: "ready" });
+});
+
+test("GET /api/v1/timetable", async () => {
+  const app = createApp();
+
+  const result = await request(app).get("/api/v1/timetable");
+
+  deepEqual(result.status, 200);
+  deepEqual(result.body, []);
+});
