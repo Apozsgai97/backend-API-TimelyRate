a5b420b353f05aa9539941006c3fa4ae1ac406a0
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-03 15:32:28 +0100

    make the fist test pass

--- src/features/rating/logic.test.ts
+++ src/features/rating/logic.test.ts
@@ -1,16 +1,16 @@
-import {test} from "node:test"
-import { countAverageRating } from "./logic"
-import { deepEqual } from "node:assert/strict"
+import { test } from "node:test";
+import { countAverageRating } from "./logic";
+import { deepEqual } from "node:assert/strict";
 
-test("With four 5 ratings it give back a five.", ()=> {
- const ratingInput = {
-  lesson1: 5,
-  lesson2: 5,
-  lesson3: 5,
-  lesson4: 5
- }
+test("With four 5 ratings it give back a five.", () => {
+  const ratingInput = {
+    lesson1: 5,
+    lesson2: 5,
+    lesson3: 5,
+    lesson4: 5,
+  };
 
- const result = countAverageRating(ratingInput)
+  const result = countAverageRating(ratingInput);
 
- deepEqual(result, 5);
-})
+  deepEqual(result, 5);
+});
--- src/features/rating/logic.ts
+++ src/features/rating/logic.ts
@@ -1,5 +1,11 @@
 import { Rating, RatingDb, RatingInput } from "./types";
 
-export function countAverageRating ({lesson1, lesson2, lesson3, lesson4}: RatingInput){
- 
+export function countAverageRating({
+  lesson1,
+  lesson2,
+  lesson3,
+  lesson4,
+}: RatingInput) {
+  const average = (lesson1 + lesson2 + lesson3 + lesson4) / 4;
+  return average;
 }
