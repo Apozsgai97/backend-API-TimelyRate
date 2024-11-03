dd6956149c1b2dbf4df8a675ecce93398183968e
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-03 16:11:49 +0100

    add test for the message function

--- src/features/rating/logic.test.ts
+++ src/features/rating/logic.test.ts
@@ -1,5 +1,5 @@
 import { test } from "node:test";
-import { calculateAverageRating } from "./logic";
+import { addMessageForAverage, calculateAverageRating } from "./logic";
 import { deepEqual } from "node:assert/strict";
 
 test("With four 5 ratings it gives back a five.", () => {
@@ -52,5 +52,60 @@ test("With ratings 4, 4, 4, 5 it gives back a four.", () => {
   const result = calculateAverageRating(ratingInput);
 
   deepEqual(result, 4);
+});
+
+test("With average 1 gets back the right message", () => {
+ const average = 1;
+
+ const result = addMessageForAverage(average);
+
+ deepEqual(
+   result,
+   "Every step counts! Keep going, tomorrow’s a new chance to shine."
+ );
+})
+
+test("With average 2 gets back the right message", () => {
+  const average = 2;
+
+  const result = addMessageForAverage(average);
+
+  deepEqual(
+    result,
+    "Great start! Small improvements add up – keep up the effort!"
+  );
+});
+
+test("With average 3 gets back the right message", () => {
+  const average = 3;
+
+  const result = addMessageForAverage(average);
+
+  deepEqual(
+    result,
+    "Nice work! You’re building momentum, keep growing each day!"
+  );
+});
+
+test("With average 4 gets back the right message", () => {
+  const average = 4;
+
+  const result = addMessageForAverage(average);
+
+  deepEqual(
+    result,
+    "Strong focus! You’re on a great path, keep up the awesome work!"
+  );
+});
+
+test("With average 5 gets back the right message", () => {
+  const average = 5;
+
+  const result = addMessageForAverage(average);
+
+  deepEqual(
+    result,
+    "Outstanding! You’re in the zone – keep reaching for the stars!"
+  );
 });
 
--- src/features/rating/logic.ts
+++ src/features/rating/logic.ts
@@ -9,3 +9,30 @@ export function calculateAverageRating({
   const average = (lesson1 + lesson2 + lesson3 + lesson4) / 4;
   return Math.round(average);
 }
+
+export function addMessageForAverage(average: number) {
+  let message;
+
+  switch (average) {
+    case 1:
+      message =
+        "Every step counts! Keep going, tomorrow’s a new chance to shine.";
+      break;
+    case 2:
+      message = "Great start! Small improvements add up – keep up the effort!";
+      break;
+    case 3:
+      message = "Nice work! You’re building momentum, keep growing each day!";
+      break;
+    case 4:
+      message =
+        "Strong focus! You’re on a great path, keep up the awesome work!";
+      break;
+    case 5:
+      message =
+        "Outstanding! You’re in the zone – keep reaching for the stars!";
+      break;
+  }
+
+  return message;
+}
